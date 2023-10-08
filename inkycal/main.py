69ec796c0de14000396a220f45c59b6abba36df8
buddel999 <buddel99+git@outlook.de>
2023-10-08 02:06:37 +0200

    Changed selenium to chromium

--- inkycal/main.py
+++ inkycal/main.py
@@ -13,9 +13,8 @@ import traceback
 from logging.handlers import RotatingFileHandler
 
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from io import BytesIO
 
 import arrow
@@ -126,12 +125,13 @@ class Inkycal:
             self._calibration_state = False
 
         #Load and intialize browser with url specified in settings
+        service = Service(executable_path="/usr/bin/chromedriver")
         options = Options()
         options.BinaryLocation = "/usr/bin/chromium-browser"
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()) ,options=options)
+        driver = webdriver.Chrome(service=service,options=options)
         driver.set_window_size(Display.get_display_size(self.settings["model"]))
 
         start_url = self.settings["mm_address"]
